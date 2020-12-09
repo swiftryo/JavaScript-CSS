8b1525b13324cd188ea3f24ff1789cdd082dbc57
ryo <68486607+swiftryo@users.noreply.github.com>
2020-12-09 11:02:53 +0900

    4-51

---
+++ 04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
@@ -0,0 +1,16 @@
+let name = 'Tom'
+
+name = 'Tim'
+console.log('hello' + name)
+
+let variable = 'str';
+variable = 12;
+variable = false;
+variable = undefined;
+
+console.log(typeof variable)
+
+// 動的型付け言語
+
+// 静的型付け言語
+int num = 12;
