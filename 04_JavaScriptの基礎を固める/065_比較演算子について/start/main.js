32d206312073fbbccea4c5ef33d1d1694dfbf57e
ryo <68486607+swiftryo@users.noreply.github.com>
2020-12-11 08:31:50 +0900

    4-59

---
+++ 04_JavaScriptの基礎を固める/065_比較演算子について/start/main.js
@@ -0,0 +1,16 @@
+const num = null;
+const bool = Boolean(num);
+console.log(bool);
+
+
+if(!num) {
+  console.log('this is true');
+} else {
+  console.log('this is false');
+}
+
+// == 型を変更してから
+// === 型も一緒
+// || or条件
+// && and条件
+// not演算子
