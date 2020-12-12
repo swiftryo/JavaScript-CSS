264a99d32f65f88d2c4a1df3d09a71c490a7e48a
ryo <68486607+swiftryo@users.noreply.github.com>
2020-12-12 17:01:11 +0900

    4-60

---
+++ 04_JavaScriptの基礎を固める/070_関数定義とアロー関数/start/main.js
@@ -0,0 +1,17 @@
+const hello = (name, age) => 40;
+  // console.log('hello' + name + age);
+  // console.log('hello' + name + age);
+const arry = [1,2,3,4,5,6];
+
+// console.log(hello());
+
+
+// hello('code mafia', 10);
+// hello('code mafia2', 20);
+// hello();
+
+// function hello(name = 'tom') {
+//   console.log('hello' + name);
+// }
+arry.forEach(value => console.log(value));
+
