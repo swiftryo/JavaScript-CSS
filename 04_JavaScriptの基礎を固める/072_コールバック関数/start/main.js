bf07f3606f28a40ac4b83cbba148d4c208d8d2c4
ryo <68486607+swiftryo@users.noreply.github.com>
2020-12-14 09:09:26 +0900

    4-61

--- 04_JavaScriptの基礎を固める/072_コールバック関数/start/main.js
+++ 04_JavaScriptの基礎を固める/072_コールバック関数/start/main.js
@@ -1,13 +1,36 @@
-function hello(callback) {
-  console.log(callback)
-  console.log('hello' + callback());
+// function hello(callback, lastname) {
+//   console.log(callback)
+//   console.log('hello' + callback(lastname));
+// }
+
+// function getname() {
+//   return 'code mafia';
+// }
+
+// // function getfirstname() {
+// //   return 'code';
+// // }
+// // hello(getname);
+
+// const getfirstname = function() {
+//   return 'code';
+// }
+
+// hello(function(name) {
+//   return 'code' + name;
+// }, 'mafia');
+
+function doSomething(a, b, callback) {
+  const result = callback(a, b);
+  console.log(result);
 }
 
-function getname() {
-  return 'code mafia';
+function multiply(a,b) {
+  return a * b;
 }
 
-function getfirstname() {
-  return 'code';
+function plus(a,b) {
+  return a + b;
 }
-hello(getname);
+doSomething(2,3,plus);
+doSomething(2,2,multiply);
